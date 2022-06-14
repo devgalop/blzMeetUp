9038b5de07d35540f62ce9d550bd2688faac8d18
devgalop <devgalop@gmail.com>
2022-06-14 11:17:50 -0500

    Add put and delete methods in Api consumer

--- MeetUpCommon/Services/ApiConsumerService.cs
+++ MeetUpCommon/Services/ApiConsumerService.cs
@@ -6,71 +6,155 @@ namespace MeetUpCommon.Services;
 
 public class ApiConsumerService : IApiConsumerService
 {
-    public ApiConsumerService()
+    private HttpClient ConfigureClient(RequestModel request)
     {
+        HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri(request.UrlBase)
+        };
 
+        if (request.HasAuthorization)
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(request.TokenType, request.AccessToken);
+        }
+        return client;
     }
 
     public async Task<ResponseModel> PostAsync(RequestModel request)
     {
-        StringContent content = new StringContent(request.Body, Encoding.UTF8, "application/json");
-        HttpClient client = new HttpClient
+        try
         {
-            BaseAddress = new Uri(request.UrlBase)
-        };
+            HttpClient client = ConfigureClient(request);
+            StringContent content = new StringContent(request.Body, Encoding.UTF8, "application/json");
+            string url = $"{request.ServicePrefix}{request.Controller}";
+            HttpResponseMessage response = await client.PostAsync(url, content);
+            string result = await response.Content.ReadAsStringAsync();
 
-        if (request.HasAuthorization)
-        {
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(request.TokenType, request.AccessToken);
+            if (!response.IsSuccessStatusCode)
+            {
+                return new ResponseModel()
+                {
+                    IsSuccess = false,
+                    Message = result
+                };
+            }
+
+            return new ResponseModel()
+            {
+                IsSuccess = true,
+                Result = result
+            };
         }
-        string url = $"{request.ServicePrefix}{request.Controller}";
-        HttpResponseMessage response = await client.PostAsync(url, content);
-        string result = await response.Content.ReadAsStringAsync();
-
-        if (!response.IsSuccessStatusCode)
+        catch (Exception ex)
         {
             return new ResponseModel()
             {
                 IsSuccess = false,
-                Message = result
+                Message = ex.Message
             };
         }
+    }
 
-        return new ResponseModel()
+    public async Task<ResponseModel> PutAsync(RequestModel request)
+    {
+        try
         {
-            IsSuccess = true,
-            Result = result
-        };
+            HttpClient client = ConfigureClient(request);
+            StringContent content = new StringContent(request.Body, Encoding.UTF8, "application/json");
+            string url = $"{request.ServicePrefix}{request.Controller}";
+            HttpResponseMessage response = await client.PutAsync(url, content);
+            string result = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return new ResponseModel()
+                {
+                    IsSuccess = false,
+                    Message = result
+                };
+            }
+
+            return new ResponseModel()
+            {
+                IsSuccess = true,
+                Result = result
+            };
+        }
+        catch (Exception ex)
+        {
+            return new ResponseModel()
+            {
+                IsSuccess = false,
+                Message = ex.Message
+            };
+        }
     }
 
     public async Task<ResponseModel> GetAsync(RequestModel request, string urlParameter)
     {
-        HttpClient client = new HttpClient
+        try
         {
-            BaseAddress = new Uri(request.UrlBase)
-        };
+            HttpClient client = ConfigureClient(request);
+            string url = $"{request.ServicePrefix}{request.Controller}/{urlParameter}";
+            HttpResponseMessage response = await client.GetAsync(url);
+            string result = await response.Content.ReadAsStringAsync();
 
-        if (request.HasAuthorization)
-        {
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(request.TokenType, request.AccessToken);
+            if (!response.IsSuccessStatusCode)
+            {
+                return new ResponseModel()
+                {
+                    IsSuccess = false,
+                    Message = result
+                };
+            }
+
+            return new ResponseModel()
+            {
+                IsSuccess = true,
+                Result = result
+            };
         }
-        string url = $"{request.ServicePrefix}{request.Controller}/{urlParameter}";
-        HttpResponseMessage response = await client.GetAsync(url);
-        string result = await response.Content.ReadAsStringAsync();
-
-        if (!response.IsSuccessStatusCode)
+        catch (Exception ex)
         {
             return new ResponseModel()
             {
                 IsSuccess = false,
-                Message = result
+                Message = ex.Message
             };
         }
+    }
 
-        return new ResponseModel()
+    public async Task<ResponseModel> DeleteAsync(RequestModel request, string urlParameter)
+    {
+        try
         {
-            IsSuccess = true,
-            Result = result
-        };
+            HttpClient client = ConfigureClient(request);
+            string url = $"{request.ServicePrefix}{request.Controller}/{urlParameter}";
+            HttpResponseMessage response = await client.DeleteAsync(url);
+            string result = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return new ResponseModel()
+                {
+                    IsSuccess = false,
+                    Message = result
+                };
+            }
+
+            return new ResponseModel()
+            {
+                IsSuccess = true,
+                Result = result
+            };
+        }
+        catch (Exception ex)
+        {
+            return new ResponseModel()
+            {
+                IsSuccess = false,
+                Message = ex.Message
+            };
+        }
     }
 }
--- MeetUpCommon/Services/IApiConsumerService.cs
+++ MeetUpCommon/Services/IApiConsumerService.cs
@@ -5,5 +5,7 @@ namespace MeetUpCommon.Services;
 public interface IApiConsumerService
 {
     Task<ResponseModel> PostAsync(RequestModel request);
+    Task<ResponseModel> PutAsync(RequestModel request);
     Task<ResponseModel> GetAsync(RequestModel request, string urlParameter);
+    Task<ResponseModel> DeleteAsync(RequestModel request, string urlParameter);
 }
