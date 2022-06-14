b9fe7f3d3027b0a21a2bd1e3b5a0fad912355fa7
devgalop <devgalop@gmail.com>
2022-06-14 15:27:27 -0500

    Add Data Notations to validate fields

--- MeetUpCommon/Models/Auth/AddUserModel.cs
+++ MeetUpCommon/Models/Auth/AddUserModel.cs
@@ -1,11 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MeetUpCommon.Models.Auth;
 
 public class AddUserModel
 {
+    [Required(ErrorMessage = "Name cannot be empty")]
     public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Last Name cannot be empty")]
     public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email cannot be empty")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
     public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email must be confirmed")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+    [Compare("Email", ErrorMessage = "Email does not match")]
+    public string ConfirmEmail { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password cannot be empty")]
     public string Password { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password must be confirmed")]
+    [Compare("Password", ErrorMessage = "Password does not match")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+
     public string PasswordHash { get; set; } = string.Empty;
+    
     public int RoleId { get; set; }
 }
--- MeetUpCommon/Models/Auth/LoginModel.cs
+++ MeetUpCommon/Models/Auth/LoginModel.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MeetUpCommon.Models.Auth;
 
 public class LoginModel
 {
+    [Required(ErrorMessage = "Email cannot be empty")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
     public string Email { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Password cannot be empty")]
     public string Password { get; set; } = string.Empty;
 }
